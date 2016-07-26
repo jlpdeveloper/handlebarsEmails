8b0f1087d5b493c065b4fecd0e6e5fddf2ee915c
jlpdeveloper <pottsman482@gmail.com>
2016-07-25 20:07:36 -0500

    Updated WIll read all files in directory json_files Uses libraries

--- createEmails.js
+++ createEmails.js
@@ -1,16 +1,35 @@
 //require file system and handlebars libraries
 var fs = require('fs');
-var hbs = require('handlebars');
-//use the file system to read the test json file
-fs.readFile('json_files/test.json', 'utf8', function (err, data) {
-    if (err) throw err; // we'll not consider error handling for now
-    //parse the json from the data retrieved
-    var obj = JSON.parse(data);
-    //read in the template handlebars file and compile with handlebars
-    var template = fs.readFileSync('templates/email1.hbs', 'utf8');
-    var compiledEmail = hbs.compile(template);
-    //create the html by pushing the data parsed to the compiledEmail
-    var html = compiledEmail(obj);
-    console.log(html);
-    //console.log(obj);
+
+var readDir = require('./library/readDirectory.js');
+var hbsFileCreator = require('./library/handlebarsCreator.js')
+//make sure emails directory exists, if not make it. this is where we'll store the html
+fs.exists('emails', function(dirExists){
+    if(!dirExists){
+        fs.mkdir('emails');
+    }
 });
+var representativeData = {};
+fs.readFile('templates/representativeInfo.json', 'utf8', function(err, repData){
+    representativeData = JSON.parse(repData);
+    //call library function to read all files in a directory
+    readDir.readDirectory('json_files', function(files){
+        //loop thru all files. Current setup is each file will be one unique object.
+        //This is so code can be modular enough to be replaced with an api enpoint in 
+        //future iterations
+        files.forEach(function(file) {
+            //read the file sent back
+            fs.readFile(file, 'utf8', function(err, data){
+                if(err) throw err;  // we'll not consider error handling for now
+                //parse the json from the data retrieved
+                var obj = JSON.parse(data);
+                obj.Representative = representativeData;
+            //write data to template and to emails folder with library
+            hbsFileCreator.addDataToTemplate(obj, 'templates/'+ obj.Template +'.hbs',
+                hbsFileCreator.writeToEmailsFolder);
+            
+            });
+        }, this);
+    });
+});
+
